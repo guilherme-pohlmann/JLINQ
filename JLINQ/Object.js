9af9f94109a02e0c205663b67ba1d3646e63e9f0
Guilherme Pohlmann da Rosa <guilherme.rosa@cwi.com.br>
2016-03-31 21:31:34 -0300

    Implementadas funções de hashing.

---
+++ JLINQ/Object.js
@@ -0,0 +1,56 @@
+﻿String.prototype.getHashCode = function () {
+    var hash = 0, i, chr, len;
+
+    if (this.length === 0) {
+        return hash;
+    }
+    for (i = 0, len = this.length; i < len; i++) {
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0;
+    }
+    return hash;
+};
+
+Number.prototype.getHashCode = function () {
+    var x = this;
+
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x);
+
+    return x;
+};
+
+Object.prototype.getHashCode = function () {
+    var hash1, hash2;
+
+    for (var prop in this) {
+        var value = this[prop];
+        var type = typeof value;
+
+        if (value && value.getHashCode && type !== "object" && type !== "function") {
+            if (!hash1) {
+                hash1 = value.getHashCode();
+            }
+            else if (!hash2) {
+                hash2 = value.getHashCode();
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    if (hash1 && hash2) {
+        return (hash1 | hash2).getHashCode();
+    }
+    else if (hash1) {
+        return hash1.getHashCode();
+    }
+    return 0;
+};
+
+Date.prototype.getHashCode = function () {
+    return this.getTime();
+};
---
+++ JLINQ/specs/ObjectSpecsSpecSpecs.js
@@ -0,0 +1,105 @@
+﻿/// <reference path="..\Object.js" />
+
+describe('Object Tests', function () {
+
+    it('Number hashsing equals', function () {
+
+        var a = (Math.random() * 99999999);
+        var ah1 = a.getHashCode();
+        var ah2 = a.getHashCode();
+
+        expect(ah1).toBe(ah2);
+    });
+
+    it('Number hashsing not equals', function () {
+
+        var a = (Math.random() * 99999999);
+        var b = (Math.random() * 99999999);
+        var ah1 = a.getHashCode();
+        var ah2 = b.getHashCode();
+
+        expect(ah1 !== ah2).toBe(true);
+    });
+
+    it('String hashsing equals', function () {
+
+        var a = 'test string';
+        var ah1 = a.getHashCode();
+        var ah2 = a.getHashCode();
+
+        expect(ah1).toBe(ah2);
+    });
+
+    it('String hashsing not equals', function () {
+
+        var a = 'test string 2';
+        var b = 'test string 3';
+        var ah1 = a.getHashCode();
+        var ah2 = b.getHashCode();
+
+        expect(ah1 !== ah2).toBe(true);
+    });
+
+    it('Date hashsing equals', function () {
+
+        var a = new Date();
+        var ah1 = a.getHashCode();
+        var ah2 = a.getHashCode();
+
+        expect(ah1).toBe(ah2);
+
+        a = new Date('2016-01-01');
+        ah1 = a.getHashCode();
+        ah2 = a.getHashCode();
+
+        expect(ah1).toBe(ah2);
+    });
+
+    it('Date hashsing not equals', function () {
+
+        var a = new Date('2016-01-01 12:25:10');
+        var b = new Date('2016-01-01 12:25:11');
+        var ah1 = a.getHashCode();
+        var ah2 = b.getHashCode();
+
+        expect(ah1 !== ah2).toBe(true);
+    });
+
+    it('Object hashsing equals', function () {
+
+        var a = {
+            str: 'test string',
+            number: (Math.random() * 99999999)
+        };
+        var ah1 = a.getHashCode();
+        var ah2 = a.getHashCode();
+
+        expect(ah1).toBe(ah2);
+
+        var b = {
+            str: 'test string',
+            number: a.number
+        };
+        var bh1 = b.getHashCode();
+
+        expect(ah1).toBe(bh1);
+    });
+
+    it('Object hashsing not equals', function () {
+
+        var a = {
+            str: 'test string2',
+            number: (Math.random() * 99999999)
+        };
+        var ah1 = a.getHashCode();
+
+        var b = {
+            str: 'test string3',
+            number: (Math.random() * 99999999)
+        };
+        var bh1 = b.getHashCode();
+
+        expect(ah1 !== bh1).toBe(true);
+    });
+});
+
